e8cdbc24ed2b3f90798b8eb1afc0c3c5abd15a8d
Franklin Webber <franklin.webber@gmail.com>
2013-07-10 12:18:26 -0600

    Updated example

--- scrabble-score/example.js
+++ scrabble-score/example.js
@@ -1,12 +1,9 @@
 Scrabble = {
   score: function(word) {
-    if (word == null) {
-      word = "";
-    }
+    word = word || "";
+    word = word.toLowerCase();
 
     var sum = 0;
-
-    word = word.toLowerCase();
 
     for (var i = 0; i < word.length; i++) {
       var letter = word[i];
@@ -16,13 +13,13 @@ Scrabble = {
 
     return sum;
   },
-  letterScores: function(letter) {
-    return { 'a' : 1, 'e' : 1, 'i' : 1, 'o' : 1,
-             'u' : 1, 'l' : 1, 'n' : 1, 'r' : 1,
-             's' : 1, 't' : 1, 'd' : 2, 'g' : 2,
-             'b' : 3, 'c' : 3, 'm' : 3, 'p' : 3,
-             'f' : 4, 'h' : 4, 'v' : 4, 'w' : 4,
-             'y' : 4, 'k' : 5, 'j' : 8, 'x' : 8,
-             'q' : 10, 'z' : 10 };
+  letterScores: function() {
+    return { a : 1, e : 1, i : 1, o : 1,
+             u : 1, l : 1, n : 1, r : 1,
+             s : 1, t : 1, d : 2, g : 2,
+             b : 3, c : 3, m : 3, p : 3,
+             f : 4, h : 4, v : 4, w : 4,
+             y : 4, k : 5, j : 8, x : 8,
+             q : 10, z : 10 };
   }
 };
